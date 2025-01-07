2ef216ae2887b8ba1a68097dc55b0b41c229cd69
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-07 12:36:20 +0000

    markup for register

--- pages/register.vue
+++ pages/register.vue
@@ -3,30 +3,32 @@
         <!-- side bar -->
 
         <div class=" w-[516px] h-full bg-zinc-900 p-8">
-            <Logo />
-            <h1 class="text-white font-bold text-lg mt-6">Sign up for a free account</h1>
-            <p class="text-zinc-300 text-sm leading-6">Already regitered? <span
-                    class=" font-bold text-[#FFAC00] underline">Log
-                    in</span> to
-                Your account
-            </p>
-            <div class="mt-11">
-                <label class="text-zinc-300  text-sm block mb-2 " for="">Email Address</label>
-                <input type="email "
-                    class="block w-full rounded-md text-white p-3 border border-[#3F3F46] bg-[#27272A] placeholder:text-zinc-500"
-                    placeholder="you@example.com">
-            </div>
+            <div class="p-8 m-6">
+                <Logo />
+                <h1 class="text-white font-bold text-lg mt-6">Sign up for a free account</h1>
+                <p class="text-zinc-300 text-sm leading-6">Already regitered? <span
+                        class=" font-bold text-[#FFAC00] underline">Log
+                        in</span> to
+                    Your account
+                </p>
+                <div class="mt-11">
+                    <label class="text-zinc-300  text-sm block mb-2 " for="">Email Address</label>
+                    <input type="email "
+                        class="block w-full rounded-md text-white p-2 border border-[#3F3F46] bg-[#27272A] placeholder:text-zinc-500"
+                        placeholder="you@example.com">
+                </div>
 
-            <div class="mt-7">
-                <label class="text-zinc-300  text-sm block mb-2 " for="">Password</label>
-                <input type="password"
-                    class="block w-full rounded-md text-white p-3 border border-[#3F3F46] bg-[#27272A] placeholder:text-zinc-500"
-                    placeholder="*********">
-            </div>
-            <div class="">
-                <button class="bg-[#FFAC00]  font-bold w-full flex justify-center mt-8 p-4 rounded-3xl">
-                    <sign />
-                </button>
+                <div class="mt-7">
+                    <label class="text-zinc-300  text-sm block mb-2 " for="">Password</label>
+                    <input type="password"
+                        class="block w-full rounded-md text-white p-2 border border-[#3F3F46] bg-[#27272A] placeholder:text-zinc-500"
+                        placeholder="*********">
+                </div>
+                <div class="">
+                    <button class="bg-[#FFAC00]  font-bold w-full flex justify-center mt-8 p-4 rounded-3xl">
+                        <sign />
+                    </button>
+                </div>
             </div>
         </div>
         <!-- /sidebar -->
