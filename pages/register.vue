dab7319b564529b0a8130a6cd1bbaff002a5e946
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-07 11:42:39 +0000

    component

---
+++ pages/register.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class=" flex bg-black h-screen">
+        <!-- side bar -->
+
+        <div class=" w-[516px] h-full bg-zinc-900">
+            <Logo />
+        </div>
+        <!-- /sidebar -->
+
+        <!-- Introduction -->
+        <div>
+            <div class="text-white">
+                some content
+            </div>
+        </div>
+        <!-- /Introduction -->
+    </div>
+</template>
