81efae6daeabd42bcc2b152355e697ede4f321e9
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-07 13:09:56 +0000

    responsiveness

--- pages/register.vue
+++ pages/register.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class=" flex bg-black h-screen">
+    <div class=" grid grid-cols-1 bg-zinc-900 h-1/2 md:bg-black md:h-full  md:flex md:h-screen">
         <!-- side bar -->
 
-        <div class=" w-[516px] h-full bg-zinc-900 p-8">
-            <div class="p-8 m-6">
+        <div class=" w-[409px] md:w-[516px] h-full  bg-zinc-900 p-8">
+            <div class=" p- m-[50px]">
                 <Logo />
                 <h1 class="text-white font-bold text-lg mt-6">Sign up for a free account</h1>
                 <p class="text-zinc-300 text-sm leading-6">Already regitered? <span
@@ -35,8 +35,23 @@
 
         <!-- Introduction -->
         <div>
-            <div class="text-white">
-                some content
+            <div class="p-6 m-[200px]  align-center justify-center items-center  block">
+                <div
+                    class="md:text-white md:tracking-tighter md:block hidden md:font-light md:w-[437.1px] md:h-[380px]">
+                    December 3rd, 2024 <br>
+
+                    Just got back from the most surreal night shift at the hospital. A full moon always brings out the
+                    interesting cases, but last night was something else. Had a patient come in convinced they'd been
+                    bitten by a werewolf - turned out to be their neighbor's very large, very normal German Shepherd.
+                    Can't blame them though; at 3 AM, everything feels a bit supernatural. Dr. Chen and I couldn't stop
+                    laughing about it during our coffee break.
+
+                    The sunrise from the hospital rooftop made the whole crazy night worth it. There's this magical
+                    moment just before the city wakes up, when everything is bathed in soft purple light. Watched a
+                    flock of birds sweep across the sky while finishing my last cup of coffee. Even after five years,
+                    these quiet moments still remind me why I chose this path. Though next time, I'm definitely bringing
+                    my thermal socks - December mornings are no joke!
+                </div>
             </div>
         </div>
         <!-- /Introduction -->
