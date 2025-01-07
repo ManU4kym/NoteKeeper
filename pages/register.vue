ea3c85b96b5b62d565e28c9cdac906aa21b5b8d2
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-07 12:26:17 +0000

    button component

--- pages/register.vue
+++ pages/register.vue
@@ -12,15 +12,21 @@
             </p>
             <div class="mt-11">
                 <label class="text-zinc-300  text-sm block mb-2 " for="">Email Address</label>
-                <input type="email " class="block w-full rounded-md text-white p-3 border border-[#3F3F46] bg-[#27272A]"
+                <input type="email "
+                    class="block w-full rounded-md text-white p-3 border border-[#3F3F46] bg-[#27272A] placeholder:text-zinc-500"
                     placeholder="you@example.com">
             </div>
 
             <div class="mt-7">
                 <label class="text-zinc-300  text-sm block mb-2 " for="">Password</label>
                 <input type="password"
-                    class="block w-full rounded-md text-white p-3 border border-[#3F3F46] bg-[#27272A]"
+                    class="block w-full rounded-md text-white p-3 border border-[#3F3F46] bg-[#27272A] placeholder:text-zinc-500"
                     placeholder="*********">
+            </div>
+            <div class="">
+                <button class="bg-[#FFAC00]  font-bold w-full flex justify-center mt-8 p-4 rounded-3xl">
+                    <sign />
+                </button>
             </div>
         </div>
         <!-- /sidebar -->
