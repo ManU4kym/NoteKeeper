e9f9bed8c23b13c1251c76b8321fffc61ea01e5e
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-08 11:07:30 +0000

    password hashing

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,16 +1,19 @@
 import prisma from "~/lib/prisma"
-
+import bcrypt from 'bcryptjs'
 
 export default defineEventHandler ( async (event) => {
     const body = await readBody(event)
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(body.password, salt)
+
     await prisma.user.create({
         data: {
             
             email: body.email,
-            password: body.password,
+            password: hashedPassword,
         },
     })
-    console.log(body)
+    
     return { data: 'Success 💐'}
     
 }) 
