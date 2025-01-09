266b5f130e19cf22dd379732a0076b14650fc186
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-09 20:40:27 +0000

    token security

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,22 @@
-export default defineNuxtRouteMiddleware ((event) => {
-    console.log("Middleware fired")
+
+
+export default defineNuxtRouteMiddleware (async (event) => {
+
+    if (process.client) return
+    const{ $verifyJwtToken } = useNuxtApp()
+
+  const cooks = useCookie('NoteKeepJWT')
+
+  if (!cooks.value) {
+   return navigateTo('/register')
+  }
+
+ try { 
+  await $verifyJwtToken(cooks.value, process.env.JWT_SECRET_KEY)
+ } catch (err) {
+    navigateTo('/register')
+    
+ 
+ }
+  console.log(cooks)
 })
