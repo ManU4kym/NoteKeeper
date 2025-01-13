d417ad503307db0755686580dfb17710a66c5684
@Versatilekim <emmannjoroge36@gmail.com>
2025-01-13 15:04:45 +0000

    dashboard front development

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,37 @@
 <template>
-    <div class=" grid grid-cols-1 bg-zinc-900 h-1/2 md:bg-black md:h-full  md:flex md:h-screen">
+    <div class=" grid grid-cols-1 bg-zinc-900 h-1/2 md:bg-zinc-900 md:h-full  md:flex md:h-screen">
         <!-- side bar -->
 
-        <div class="  md:w-[516px] h-full  bg-zinc-900 p-8 flex flex-col justify-center">
-            <div class=" p- m-[50px]">
+        <div class="  md:w-[316px] h-full bg-black  p-8">
+            <div class="">
                 <Logo />
-                <h1 class="text-white font-bold text-lg mt-6">Sign up for a free account</h1>
-                <p class="text-zinc-300 text-sm leading-6">Already regitered? <NuxtLink to="/login"
-                        class=" font-bold text-[#FFAC00] underline">Log
-                        in</NuxtLink> to
-                    Your account
-                </p>
+                <p class="text-[#C2C2C5] mt-[58px] text-xs mb-4 font-bold">Today</p>
+
+                <div class="ml-2 bg-[#A1842C] rounded-lg">
+                    <div class="p-2 ">
+                        <h3 class="text-[#F4F4F5]  text-sm font-bold">Just finished reading</h3>
+
+                    </div>
+
+                    <div class="leading p-2-none ml-2">
+                        <span class="text-xs text-[#F4F4F5] mr-4">Today</span>
+                        <span class="text-xs text-[#D6D6D6]">The Midnight Library...</span>
+
+                    </div>
+                </div>
+                <div class="ml-2">
+                    <div class="p-2">
+                        <h3 class="text-[#F4F4F5] text-sm font-bold">Just finished reading</h3>
+
+                    </div>
+
+                    <div class="leading-none ml-2">
+                        <span class="text-xs text-[#F4F4F5] mr-4">Today</span>
+                        <span class="text-xs text-[#D6D6D6]">The Midnight Library...</span>
+
+                    </div>
+                </div>
+
 
             </div>
         </div>
